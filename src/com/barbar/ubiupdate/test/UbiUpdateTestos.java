cb88c4c01876a65fb21109408f4cfc7719ab2ffa
firas achkar <fachkar.datawind@gmail.com>
2013-07-04 16:01:27 -0400

    added ActivityInstrumentationTestCase2 based tests
    still having issue with UbiUpdateActivity ubiUpdateActivityStarted =
    (UbiUpdateActivity) monitor.waitForActivityWithTimeout(5000);

--- src/com/barbar/ubiupdate/test/UbiUpdateTestos.java
+++ src/com/barbar/ubiupdate/test/UbiUpdateTestos.java
@@ -1,16 +1,22 @@
 package com.barbar.ubiupdate.test;
 
-import android.content.Intent;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.IntentFilter;
 import android.content.res.Resources;
-import android.test.ActivityUnitTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 import android.widget.Button;
 import com.barbar.ubiupdate.UbiUpdateActivity;
 
-public class UbiUpdateTestos extends ActivityUnitTestCase<UbiUpdateActivity> {
+public class UbiUpdateTestos extends ActivityInstrumentationTestCase2<UbiUpdateActivity> {
 
     private int chkNowButtonId;
     private UbiUpdateActivity ubiUpdateActivity;
+    private Instrumentation mInstrumentation;
 
     public UbiUpdateTestos(String name) {
         super(UbiUpdateActivity.class);
@@ -22,37 +28,46 @@ public class UbiUpdateTestos extends ActivityUnitTestCase<UbiUpdateActivity> {
 
     protected void setUp() throws Exception {
         super.setUp();
-        Intent intent = new Intent(getInstrumentation().getTargetContext(), UbiUpdateActivity.class);
-        startActivity(intent, null, null);
+        setActivityInitialTouchMode(false);
         ubiUpdateActivity = getActivity();
+        mInstrumentation = getInstrumentation();
+
     }
 
     protected void tearDown() throws Exception {
         super.tearDown();
+        
     }
-    
+
     @SmallTest
-    public void testAppName() {
+    public void testUIActivity() {
+        assertNotNull("UbiUpdate is null", ubiUpdateActivity);
+        assertNotNull("Instrumentation is null", mInstrumentation);
+        
+        
+        
         Resources reso = ubiUpdateActivity.getResources();
         assertEquals("Incorrect label of the button", "UbiUpdate", reso.getText(reso.getIdentifier("app_name", "string", ubiUpdateActivity.getPackageName())));
-    }
+        
+        
+        // Add monitor to check for the UbiUpdate activity
+        ActivityMonitor monitor = mInstrumentation.addMonitor(UbiUpdateActivity.class.getName(), null, false);
 
-    @SmallTest
-    public void testLayout() {
+        // Wait 5 seconds for the start of the activity
+        UbiUpdateActivity ubiUpdateActivityStarted = (UbiUpdateActivity) monitor.waitForActivityWithTimeout(5000);
 
+        assertNotNull("UbiUpdate Activity not started", ubiUpdateActivityStarted);
+        
         chkNowButtonId = com.barbar.ubiupdate.R.id.check_now_button;
-        assertNotNull(ubiUpdateActivity.findViewById(chkNowButtonId));
-        Button view = (Button) ubiUpdateActivity.findViewById(chkNowButtonId);
-        assertEquals("Incorrect label of the button", "Check now", view.getText());
-    }
-
-    @SmallTest
-    public void testIntentTriggerViaOnClick() {
-        chkNowButtonId = com.barbar.ubiupdate.R.id.check_now_button;
-        Button ButtonView = (Button) ubiUpdateActivity.findViewById(chkNowButtonId);
-        assertNotNull("Button not allowed to be null", ButtonView);
-
-        // You would call the method directly via
+        Button clickNowButtonView = (Button) ubiUpdateActivity.findViewById(chkNowButtonId);
+        assertNotNull("Button not allowed to be null", clickNowButtonView);        
+        ViewAsserts.assertOnScreen(ubiUpdateActivityStarted.getWindow().getDecorView(), clickNowButtonView);
+        assertEquals("Incorrect label of the button", "Check now", clickNowButtonView.getText());
+        
+        TouchUtils.clickView(this, clickNowButtonView);
+        getInstrumentation().waitForIdleSync();
+        
+        ubiUpdateActivity.finish();
         
     }
 
